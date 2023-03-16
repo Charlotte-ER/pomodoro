fa35f73c405e6902c031030cfada2faabbb55cde
Charlotte-ER <68346113+Charlotte-ER@users.noreply.github.com>
2023-03-16 08:05:21 +0000

    Fix bug so countdown stops at 0.

--- pomodoro.js
+++ pomodoro.js
@@ -1,23 +1,22 @@
-let minutes = 25;
-
-function countdown() {
-if (minutes === 0) {
-        document.querySelector('h1').innerHTML = 'Time for a break!';
-        document.querySelector('button').disabled = false;
-    }
-    else {
-        minutes--;
-        document.querySelector('h1').innerHTML = minutes;
-    }
-}
+let minutes = 10;
 
 document.addEventListener('DOMContentLoaded', function() {
 
-document.querySelector('h1').innerHTML = minutes;
+    document.querySelector('h1').innerHTML = minutes;
 
-document.querySelector('button').onclick = function() {
-    document.querySelector('button').disabled = true;
+    document.querySelector('button').onclick = function() {
+        document.querySelector('button').disabled = true;
 
-    setInterval(countdown, 1000); // bug: continues beyond countdowm finishing!
-}
+        let countdown = setInterval(function() {
+            if (minutes === 0) {
+                document.querySelector('h1').innerHTML = 'Time for a break!';
+                clearInterval(countdown);
+                document.querySelector('button').disabled = false;
+            }
+            else {
+                minutes--;
+                document.querySelector('h1').innerHTML = minutes;
+            }
+        }, 1000);
+    }
 })
